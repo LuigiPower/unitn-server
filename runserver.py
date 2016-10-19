9ba5f564da94916add51cbcf065212d402715f7e
emanuele <emanuelevgl@yahoo.it>
2016-10-19 23:09:08 +0200

    add docker support and error handler

--- runserver.py
+++ runserver.py
@@ -7,10 +7,15 @@ app = Flask(__name__)
 def pastolesto():
 	canteen = Canteen()
 	menu = canteen.getTodayMenu()
-	menu_dic = [{'name': menu[0][0], 'calorie': menu[0][1]},
-				{'name': menu[1][0], 'calorie': menu[1][1]},
-				{'name': menu[2][0], 'calorie': menu[2][1]}]
-	return json.dumps(menu_dic), 200, {'Content-Type': 'application/json;'}
+	print(menu)
+	if menu == None:
+		err_resp = {"message": "no food today"}
+		return json.dumps(err_resp)
+	else:
+		menu_dic = [{'name': menu[0][0], 'calorie': menu[0][1]},
+					{'name': menu[1][0], 'calorie': menu[1][1]},
+					{'name': menu[2][0], 'calorie': menu[2][1]}]
+		return json.dumps(menu_dic), 200, {'Content-Type': 'application/json;'}
 
 if __name__ == "__main__":
     app.run()
